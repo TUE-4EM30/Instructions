b02048b8f9c0bf1183e9927a8f9504a672e71868
Verhoosel <CVerhoose@campus.tue.nl>
2016-02-18 14:23:35 +0100

    Added code for Instruction 3

---
+++ Instruction3/main.py
@@ -0,0 +1,18 @@
+from pqlib import PriorityItem, PriorityQueue
+
+#Initialize patients priority queue
+patients = PriorityQueue
+
+#Registering some patients
+patients.put( PriorityItem('Patient 1',2) )
+patients.put( PriorityItem('Patient 2',5) )
+patients.put( PriorityItem('Patient 3',3) )
+patients.put( PriorityItem('Patient 4',5) )
+patients.put( PriorityItem('Patient 5',8) )
+patients.put( PriorityItem('Patient 6',9) )
+patients.put( PriorityItem('Patient 7',1) )
+patients.put( PriorityItem('Patient 8',4) )
+
+#The doctor receives the patients
+while not patients.isEmpty():
+    print "The doctor receives %d" % patients.getPrio()
---
+++ Instruction3/pqlib.py
@@ -0,0 +1,46 @@
+class PriorityItem:
+
+    def __init__ ( self, item, priority ):
+        assert 0<priority<10, 'Priority value should be in range 1-9' 
+       self.item = item
+       self.prio = priority
+
+    def __cmp__ ( self, other ):
+        assert isinstance( other, PriorityItem )
+        return other.prio-self.prio
+
+    def __str__ ( self ):
+        return '%s (priority: %d)' % (self.item,self.prio)        
+
+class PriorityQueue:
+
+    def __init__ ( self ):
+        self.__sortedlist = [] #A list with ascending priorities
+
+    ## Empty the collection
+    def clear ( self ):
+        self.__sortedlist = []
+
+    ## Check if the collection is empty
+    def isEmpty ( self ):
+        return len(self.__sortedlist)=0
+    
+    ## Remove and return the item with the highest priority
+    def getPrio ( self ):
+        return self.sortedlist.pop()
+    
+    ## Add an item with a given priority
+    def put ( self, priority_item ):
+        assert isinstance( priority_item, PriorityItem )
+        
+        #Start with the last index (highest priority)
+        index_lower = len(self.__sortedlist)
+    
+        #Decrease the index until an item with a lower priority is encountered
+        while index_lower >= 0
+            if self.__sortedlist[index_lower] < priority_item:
+                break
+            index_lower -= 1
+
+        #Insert the priority_item after the index
+        self.__sortedlist.insert( index_lower+1, priority_item )
