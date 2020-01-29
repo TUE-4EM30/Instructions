2a6146ebb4ec1cc0e6bf2d68cbc00f8a9396257f
sora <210at85@gmail.com>
2020-01-29 14:44:08 +0100

    reformat Instruction3

--- Instruction3/main.py
+++ Instruction3/main.py
@@ -4,15 +4,15 @@ from pqlib import PriorityItem, PriorityQueue
 patients = PriorityQueue
 
 #Registering some patients
-patients.put( PriorityItem('Patient 1',2) )
-patients.put( PriorityItem('Patient 2',5) )
-patients.put( PriorityItem('Patient 3',3) )
-patients.put( PriorityItem('Patient 4',5) )
-patients.put( PriorityItem('Patient 5',8) )
-patients.put( PriorityItem('Patient 6',9) )
-patients.put( PriorityItem('Patient 7',1) )
-patients.put( PriorityItem('Patient 8',4) )
+patients.put(PriorityItem('Patient 1', 2))
+patients.put(PriorityItem('Patient 2', 5))
+patients.put(PriorityItem('Patient 3', 3))
+patients.put(PriorityItem('Patient 4', 5))
+patients.put(PriorityItem('Patient 5', 8))
+patients.put(PriorityItem('Patient 6', 9))
+patients.put(PriorityItem('Patient 7', 1))
+patients.put(PriorityItem('Patient 8', 4))
 
 #The doctor receives the patients
 while not patients.isEmpty():
-    print( "The doctor receives {}".format(patients.getPrio()) )
+    print("The doctor receives {}".format(patients.getPrio()))
